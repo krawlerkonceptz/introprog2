4a60ad6efee8df72b864f5e5f52a2cec15785795
krawlerkonceptz <116011604+krawlerkonceptz@users.noreply.github.com>
2022-11-21 18:26:41 -0400

    Add files via upload

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTopicName = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnData = new System.Windows.Forms.Button();
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.picSadie = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtMultiplier = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSadie)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTopicName);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnHide);
+            this.groupBox1.Controls.Add(this.btnData);
+            this.groupBox1.Controls.Add(this.btnPicture);
+            this.groupBox1.Controls.Add(this.picSadie);
+            this.groupBox1.Controls.Add(this.lblData);
+            this.groupBox1.Location = new System.Drawing.Point(285, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 316);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            this.groupBox1.UseWaitCursor = true;
+        
+            // 
+            // lblTopicName
+            // 
+            this.lblTopicName.AutoSize = true;
+            this.lblTopicName.Location = new System.Drawing.Point(116, 31);
+            this.lblTopicName.Name = "lblTopicName";
+            this.lblTopicName.Size = new System.Drawing.Size(177, 16);
+            this.lblTopicName.TabIndex = 8;
+            this.lblTopicName.Text = "The Dog Ate My Homework";
+            this.lblTopicName.UseWaitCursor = true;
+       
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(345, 275);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 35);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseWaitCursor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(232, 275);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(77, 35);
+            this.btnHide.TabIndex = 6;
+            this.btnHide.Text = "&Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.UseWaitCursor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(119, 275);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(77, 35);
+            this.btnData.TabIndex = 5;
+            this.btnData.Text = "&Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.UseWaitCursor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Location = new System.Drawing.Point(6, 275);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(77, 35);
+            this.btnPicture.TabIndex = 0;
+            this.btnPicture.Text = "&Picture";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.UseWaitCursor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // picSadie
+            // 
+            this.picSadie.Image = ((System.Drawing.Image)(resources.GetObject("picSadie.Image")));
+            this.picSadie.Location = new System.Drawing.Point(34, 50);
+            this.picSadie.Name = "picSadie";
+            this.picSadie.Size = new System.Drawing.Size(354, 219);
+            this.picSadie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picSadie.TabIndex = 0;
+            this.picSadie.TabStop = false;
+            this.picSadie.UseWaitCursor = true;
+            this.picSadie.Visible = false;
+            this.picSadie.Click += new System.EventHandler(this.picSadie_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnReset);
+            this.groupBox2.Controls.Add(this.btnCalculate);
+            this.groupBox2.Controls.Add(this.txtNumber);
+            this.groupBox2.Controls.Add(this.txtMultiplier);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.lblAnswer);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 316);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Multiplier";
+            this.groupBox2.UseWaitCursor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(138, 275);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(77, 35);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.UseWaitCursor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(34, 275);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(77, 35);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.UseWaitCursor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.AcceptsTab = true;
+            this.txtNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNumber.Location = new System.Drawing.Point(115, 31);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 23);
+            this.txtNumber.TabIndex = 0;
+            this.txtNumber.UseWaitCursor = true;
+           
+            // 
+            // txtMultiplier
+            // 
+            this.txtMultiplier.AcceptsTab = true;
+            this.txtMultiplier.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMultiplier.Location = new System.Drawing.Point(115, 107);
+            this.txtMultiplier.Name = "txtMultiplier";
+            this.txtMultiplier.Size = new System.Drawing.Size(100, 23);
+            this.txtMultiplier.TabIndex = 1;
+            this.txtMultiplier.UseWaitCursor = true;
+           
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Multiplier";
+            this.label2.UseWaitCursor = true;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Location = new System.Drawing.Point(115, 180);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(100, 23);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Answer";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number";
+            this.label1.UseWaitCursor = true;
+            // 
+            // lblData
+            // 
+            this.lblData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblData.Location = new System.Drawing.Point(35, 51);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(352, 217);
+            this.lblData.TabIndex = 9;
+            this.lblData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblData.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPicture;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(898, 413);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab 2 by Michelle Boutilier";
+            this.UseWaitCursor = true;
+         
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picSadie)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox picSadie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtMultiplier;
+        private System.Windows.Forms.Label lblTopicName;
+        private System.Windows.Forms.Label lblData;
+    }
+}
+
